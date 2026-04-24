0c1b0d81558a1a4ed4dac622211ba8134c70c3ae
Kathe <katta.vergara.libertad@gmail.com>
2026-04-23 21:23:43 -0400

    fix: servir  sw.js desde raiz

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
+from django.conf import settings
+
+def service_worker(request):
+    sw_path = settings.BASE_DIR / 'gastos' / 'static' / 'sw.js'
+    with open(sw_path, 'r') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='application/javascript')
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #panel de administración de Django
-    path('', include('gastos.urls')), #incluye las URLs de la aplicación "gastos" en la URL raíz del proyecto
-    path('reportes/', include('reportes.urls')),#incluye las URLs de la aplicación "reportes"
-    path('alertas/', include('alertas.urls')), #incluye las URLs de la aplicación "alertas"
+    path('admin/', admin.site.urls),
+    path('sw.js', service_worker, name='sw'),          # ← línea nueva
+    path('', include('gastos.urls')),
+    path('reportes/', include('reportes.urls')),
+    path('alertas/', include('alertas.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='gastos/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]

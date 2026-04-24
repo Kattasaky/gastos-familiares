9f0b53380f307a21df96a1a63b446774701357e6
Kathe <katta.vergara.libertad@gmail.com>
2026-04-24 17:11:17 -0400

    feat: assetlinks y ssw.js

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.conf import settings
+import os
 
 def service_worker(request):
     sw_path = settings.BASE_DIR / 'gastos' / 'static' / 'sw.js'
@@ -10,9 +11,14 @@ def service_worker(request):
         content = f.read()
     return HttpResponse(content, content_type='application/javascript')
 
+def assetlinks(request):
+    path_file = os.path.join(settings.BASE_DIR, 'gastos', 'static', '.well-known', 'assetlinks.json')
+    return FileResponse(open(path_file, 'rb'), content_type='application/json')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sw.js', service_worker, name='sw'),          # ← línea nueva
+    path('sw.js', service_worker, name='sw'),
+    path('.well-known/assetlinks.json', assetlinks, name='assetlinks'),
     path('', include('gastos.urls')),
     path('reportes/', include('reportes.urls')),
     path('alertas/', include('alertas.urls')),
